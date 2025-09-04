61abe31e94046398ec149b9d4dcb23ccb79e869e
Fede <federicoletier@hotmail.com>
2025-09-04 12:54:20 -0300

    paso lo del .sh que no se movio

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+SERVER_PORT=$(grep -i 'SERVER_PORT' ./server/config.ini | head -n1 | cut -d'=' -f2 | tr -d ' ')
+
 RESPONSE=$(timeout 5 docker run --rm --network=tp0_testing_net alpine sh -c "\
   apk add --no-cache netcat-openbsd && \
-  echo 'ping' | nc -w 2 server 12345" | tail -n 1)
+  echo 'ping' | nc -w 2 server $SERVER_PORT" | tail -n 1)
 
 if [ "$RESPONSE" = "ping" ]; then
   echo "action: test_echo_server | result: success"
